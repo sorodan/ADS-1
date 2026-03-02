2cfde8f26a75d79682cd909d26a0d88667c01f6e
sorodan <alekseimetelkow@gmail.com>
2026-03-02 22:32:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,7 +7,7 @@ bool checkPrime(uint64_t value) {
   if (value <= 1) {
     return false;
   }
-  for (uint64_t i = 2; i <= sqrt(value); i++) {
+  for (uint64_t i = 2; i*i <= value; i++) {
     if (value % i == 0) {
       return false;
     }
